1ec21522c983d8300ec84a61f07d61b22042ece4
stephenworld <adewalestephennifemi@gmail.com>
2026-02-17 21:31:49 +0100

    feat: improve AccountingTable and EmployeeDirectory functionality
    - Added custom header actions to the AccountingTable for enhanced user interaction.
    - Integrated a department filter in the EmployeeDirectory to streamline employee searches.
    - Updated UI elements for a more intuitive user experience, including refined button styles and search placeholders.
    - Enhanced ticket management in HRServiceDesk with avatar displays for submitted tickets.

---
+++ src/components/ui/svgs.jsx
@@ -0,0 +1,157 @@
+export const Trendup = () => {
+  return (
+    <svg
+      width="95"
+      height="82"
+      viewBox="0 0 95 82"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_7840_123710)">
+        <rect
+          width="7"
+          height="26"
+          transform="matrix(-1 0 0 1 68 40)"
+          fill="#36FF1B"
+          fill-opacity="0.5"
+        />
+        <rect
+          width="7"
+          height="18"
+          transform="matrix(-1 0 0 1 61 48)"
+          fill="#36FF1B"
+          fill-opacity="0.7"
+        />
+        <rect
+          width="7"
+          height="10"
+          transform="matrix(-1 0 0 1 54 56)"
+          fill="#36FF1B"
+          fill-opacity="0.8"
+        />
+        <rect
+          width="7"
+          height="7"
+          transform="matrix(-1 0 0 1 47 59)"
+          fill="#36FF1B"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_7840_123710"
+          x="0"
+          y="0"
+          width="108"
+          height="106"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset />
+          <feGaussianBlur stdDeviation="20" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.211765 0 0 0 0 1 0 0 0 0 0.105882 0 0 0 0.4 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_7840_123710"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_7840_123710"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export const Trenddown = () => {
+  return (
+    <svg
+      width="95"
+      height="82"
+      viewBox="0 0 95 82"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_7840_123726)">
+        <rect
+          width="7"
+          height="26"
+          transform="matrix(-1 0 0 1 68 40)"
+          fill="#FF1D25"
+          fill-opacity="0.5"
+        />
+        <rect
+          width="7"
+          height="18"
+          transform="matrix(-1 0 0 1 61 48)"
+          fill="#FF1D25"
+          fill-opacity="0.7"
+        />
+        <rect
+          width="7"
+          height="10"
+          transform="matrix(-1 0 0 1 54 56)"
+          fill="#FF1D25"
+          fill-opacity="0.8"
+        />
+        <rect
+          width="7"
+          height="7"
+          transform="matrix(-1 0 0 1 47 59)"
+          fill="#FF1D25"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_7840_123726"
+          x="0"
+          y="0"
+          width="108"
+          height="106"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset />
+          <feGaussianBlur stdDeviation="20" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 0.113725 0 0 0 0 0.145098 0 0 0 0.4 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_7840_123726"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_7840_123726"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
